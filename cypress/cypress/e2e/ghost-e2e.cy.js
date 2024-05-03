f77a5876293110e47a008b9e8c5f558f6dde914c
jmblancor1 <jm.blancor1@uniandes.edu.co>
2024-05-03 07:07:18 -0500

    Cambios de pruebas Member

--- cypress/cypress/e2e/ghost-e2e.cy.js
+++ cypress/cypress/e2e/ghost-e2e.cy.js
@@ -1,7 +1,17 @@
-describe("logueo para acceder a las funcionalidades de ghost", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:2368/ghost");
-  });
+///<reference types="cypress"/>
+//Para que funcionen los comandos
+require('cypress-xpath');
+import MembersPO from '../support/PageObject/MembersPOM/MembersPO';
 
 
-});
+
+
+  /*---------------------Pruebas Members----------------------------------------------------
+  */
+  it.only('Prueba de creacion de member ya existente',()=>{
+    creationMember();
+    //Valida el mensaje de que el miembro ya exist
+    cy.get(".error > .response").should('contain', 'Member already exists')
+    
+})
+
