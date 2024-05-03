7c44f8fed536376148b7e152fb3bea5cb99f52fa
jmblancor1 <jm.blancor1@uniandes.edu.co>
2024-05-03 07:41:59 -0500

    adicion de archivo de members

--- cypress/cypress/e2e/ghost-e2e.cy.js
+++ cypress/cypress/e2e/ghost-e2e.cy.js
@@ -1,24 +1,14 @@
-
-///<reference types="cypress"/>
-//Para que funcionen los comandos
-require('cypress-xpath');
-import MembersPO from '../support/PageObject/MembersPOM/MembersPO';
-
 describe("logueo para acceder a las funcionalidades de ghost", () => {
   beforeEach(() => {
-    cy.visit("https://ghost-grupo-11.onrender.com/ghost/#/signin");
+    cy.visit("http://localhost:2368/ghost");
   });
 
-
-
-
-
-  /*---------------------Pruebas Members----------------------------------------------------
-  */
   it.only('Prueba de creacion de member ya existente',()=>{
     creationMember();
     //Valida el mensaje de que el miembro ya exist
     cy.get(".error > .response").should('contain', 'Member already exists')
     
 })
+});
+
 
