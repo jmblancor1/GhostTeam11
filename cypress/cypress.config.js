7a5700732a0e8564da748b35836446ce973667db
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-03 20:21:09 -0500

    Ajustes de configuracion en archivos package.json y cypress.config.js

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://ghost-grupo-11.onrender.com/ghost/#/signin",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
