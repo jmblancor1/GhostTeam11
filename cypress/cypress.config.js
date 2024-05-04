862bad668ffdab8cfae7875a5a05a5a8cfa2c2b9
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-03 22:35:54 -0500

    Actualizacion baseUrl en cypress.config.js

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://ghost-grupo-11.onrender.com/ghost/#/signin",
+    baseUrl: "https://ghost-grupo-11.onrender.com/ghost",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
