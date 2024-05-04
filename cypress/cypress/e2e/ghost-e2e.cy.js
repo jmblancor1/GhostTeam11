df9b3fe99ef80b91f4c3bfd57fa3429fe0a0d8a9
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-03 21:31:16 -0500

    Ajustes en archivo cypress/e2e/ghost-e2e.cy.js // root y estructura base

--- cypress/cypress/e2e/ghost-e2e.cy.js
+++ cypress/cypress/e2e/ghost-e2e.cy.js
@@ -1,14 +1,15 @@
 describe("logueo para acceder a las funcionalidades de ghost", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:2368/ghost");
+    cy.visit('/');
   });
 
-  it.only('Prueba de creacion de member ya existente',()=>{
-    creationMember();
-    //Valida el mensaje de que el miembro ya exist
-    cy.get(".error > .response").should('contain', 'Member already exists')
-    
-})
+  it("Realizar login con datos validos", () => {
+    cy.fixture("login.env.json").then((login) => {
+      cy.get("#ember6").type(login.userName);
+      cy.get("#ember8").type(login.password);
+    });
+    cy.get("#ember10 > span").click();
+  });
 });
 
 
