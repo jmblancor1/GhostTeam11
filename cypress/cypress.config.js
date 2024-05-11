576ff6a46ee78ff49343b808bcaec7727edc32f7
jmblancor1 <jm.blancor1@uniandes.edu.co>
2024-05-11 16:30:49 -0500

    Cambios de Pruebas de Posts

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,10 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://ghost-grupo-11.onrender.com/ghost",
+    //baseUrl: "https://ghost-grupo-11.onrender.com/ghost"
+    screenshotsFolder: 'cypress/screenshots',
+    baseUrl: "https://ghost-v5-8wq2.onrender.com/ghost",
+    supportFile: 'cypress/support/commands.js',
+    videosFolder: 'cypress/videos',
+    supportFile: 'cypress/support/commands.js',
+    viewportWidth: 1280,
+    viewportHeight: 800,
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
