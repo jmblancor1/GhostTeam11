76b5e236c55cfeb54f80615a13952b467b675f03
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-12 03:13:03 -0500

    Test settings metadatos

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://ghost-v5-8wq2.onrender.com/ghost",
-    // baseUrl: "https://ghost-3.onrender.com/ghost",
+    // baseUrl: "https://ghost-v5-8wq2.onrender.com/ghost",
+    baseUrl: "https://ghost-3.onrender.com/ghost",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
