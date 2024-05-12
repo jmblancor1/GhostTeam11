c26765d4f862383b7e57693ae7c1dd4fbe3ed9ed
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-12 01:14:22 -0500

    Test page: Crear titulo con mas de 255 caracteres

--- cypress/cypress/e2e/v5.68/2-pages.cy.js
+++ cypress/cypress/e2e/v5.68/2-pages.cy.js
@@ -15,9 +15,11 @@ beforeEach(() => {
 // **************************************** START TESTING PAGE ************************************************
 // ************************************************************************************************************
 
-// **************************************** NEW PAGE ************************************************
 
-describe("Acceder a la funcionalidad Pages: Crear nueva pagina", () => {
+describe("Acceder a la funcionalidad Pages", () => {
+
+  // **************************************** NEW PAGE ************************************************
+
   it("Crear una nueva página con campos completos", () => {
     cy.visit("/#/pages");
     //click boton Pagina
@@ -41,9 +43,48 @@ describe("Acceder a la funcionalidad Pages: Crear nueva pagina", () => {
     cy.get("span").contains("Publish page, right now").click();
     //validar que la página ha sido publicada
     cy.get("span").should("contain", "Boom. It’s out there.");
+    cy.wait(2000);
+    cy.visit("/#/pages");
+    cy.wait(2000);
   });
 
-  it("Acceder a la funcionalidad Pages: Eliminar una página", () => {
+
+
+  // **************************************** NEW PAGE WITH TITLE ************************************************
+
+  it("Crear una página con título superior a 255 caracteres", () => {
+    cy.visit("/#/pages");
+    //click boton Pagina
+    cy.get("a").contains("Pages").click();
+    //click boton Nueva pagina
+    cy.get("span").contains("New page").click();
+    //Digitar el contenido de la página
+    cy.get('div[data-placeholder="Begin writing your page..."]').type(
+      "En esta etapa 2, los ciclistas colombianos fueron los grandes protagonistas, debido a que la fracción tuvo tres puertos de montaña (dos de tercera y una de primera categoría)."
+    );
+    //Digitar el título de la página
+    cy.get('textarea[placeholder="Page title"]')
+      .click()
+      .type(
+        "Daniel Martínez sorprende en la etapa 2 del Giro de Italia y se mete en el top-3 de la carrera: así va la clasificación general. Dani Martínez (Bora-Hansgrohe) el que tuvo mejor rendimiento, pues logró un segundo lugar por detrás de Pogacar, llegando a 27 segundos."
+      );
+    //click boton Publicar
+    cy.get("span").contains("Publish").click();
+    //Validar el mensaje que el titulo no debe ser mayor a 255 caracteres
+    cy.get("div").should("contain", "Validation failed: Title cannot be longer than 255 characters.");
+    //Eliminar draft
+    cy.xpath("/html/body/div[2]/div/main/button").click();
+    cy.xpath(
+      "/html/body/div[2]/div/main/div/div/div/div/div[2]/form/button"
+    ).click();
+    cy.xpath("/html/body/div[5]/div/div/div[2]/button[2]").click();
+  });
+
+
+
+  // **************************************** DELETE PAGE ************************************************
+
+  it("Eliminar una página creada", () => {
     cy.visit("/#/pages");
     cy.xpath(
       "/html/body/div[2]/div/main/section/section/div[1]/ol/li[2]"
@@ -51,11 +92,11 @@ describe("Acceder a la funcionalidad Pages: Crear nueva pagina", () => {
     cy.xpath("/html/body/div[2]/div/main/button").click();
     cy.xpath(
       "/html/body/div[2]/div/main/div/div/div/div/div[2]/form/button"
-    ).click();
+      ).click();
     cy.xpath("/html/body/div[5]/div/div").should(
-      "contain",
-      "Are you sure you want to delete this "
-    );
+        "contain",
+        "Are you sure you want to delete this "
+        );
     cy.xpath("/html/body/div[5]/div/div/div[2]/button[2]").click();
     cy.get("h3").should("contain", "About this site");
   });
