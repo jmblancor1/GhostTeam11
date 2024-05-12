2f7ab103ebf15cc64b609b883de169ce78726337
marvil98 <m.villafanep@uniandes.edu.co>
2024-05-11 20:57:18 -0500

    Add features for tags in v3

--- cypress/cypress/e2e/v3.42/4-members.cy.js
+++ cypress/cypress/e2e/v3.42/4-members.cy.js
@@ -20,24 +20,24 @@ describe("Login y escenarios para members", () => {
         const email = `test${Math.floor(Math.random() * 100000)}@gmail.com`;
         cy.wait(1000);
         cy.visit("/#/members");
-        cy.screenshot('/v3/caso16/1-visit-members');
+        cy.screenshot('/v3.42/caso16/1-visit-members');
         cy.wait(1000);
         cy.get('a[href="#/members/new/"].gh-btn')
         .contains('New member')
         .click();
-        cy.screenshot('/v3/caso16/2-new-member');
+        cy.screenshot('/v3.42/caso16/2-new-member');
         cy.get('input[name="name"][id="member-name"]').scrollIntoView().type('Nuevo miembro Grupo # 11', { force: true });
-        cy.screenshot('/v3/caso16/3-type-member-name');
+        cy.screenshot('/v3.42/caso16/3-type-member-name');
         cy.get('input[name="email"][id="member-email"]').type(email);
-        cy.screenshot('/v3/caso16/4-type-member-email');
+        cy.screenshot('/v3.42/caso16/4-type-member-email');
         cy.get('textarea[name="note"][id="member-note"]').type('Esta es la descripción del nuevo miembro del Grupo # 11');
-        cy.screenshot('/v3/caso16/5-type-member-note');
+        cy.screenshot('/v3.42/caso16/5-type-member-note');
         cy.wait(1000);
         cy.get("span").contains("Save").click();
-        cy.screenshot('/v3/caso16/6-click-save');
+        cy.screenshot('/v3.42/caso16/6-click-save');
         cy.wait(3000);
         cy.visit("/#/members");
-        cy.screenshot('/v3/caso16/7-visit-members');
+        cy.screenshot('/v3.42/caso16/7-visit-members');
         cy.wait(1000);
         cy.reload();
         cy.wait(5000);
@@ -46,7 +46,7 @@ describe("Login y escenarios para members", () => {
             cy.wrap(emailElement).invoke('text').then((memberEmailText) => {
                 if (memberEmailText.trim() === email.trim()) {
                 cy.wrap(emailElement).click();
-                cy.screenshot('/v3/caso16/8-click-member-created');
+                cy.screenshot('/v3.42/caso16/8-click-member-created');
                 cy.log(`Se hizo clic en el miembro con el correo electrónico "${email}".`);
                 }
             });
@@ -57,24 +57,24 @@ describe("Login y escenarios para members", () => {
         const email = `miembro-e11-a-eliminar@gmail.com`;
         cy.wait(1000);
         cy.visit("/#/members");
-        cy.screenshot('/v3/caso17/1-visit-members');
+        cy.screenshot('/v3.42/caso17/1-visit-members');
         cy.wait(1000);
         cy.get('a[href="#/members/new/"].gh-btn')
             .contains('New member')
             .click();
-        cy.screenshot('/v3/caso17/2-new-member');
+        cy.screenshot('/v3.42/caso17/2-new-member');
         cy.get('input[name="name"][id="member-name"]').scrollIntoView().type('Nuevo miembro Grupo # 11 a eliminar', { force: true });
-        cy.screenshot('/v3/caso17/3-type-member-name');
+        cy.screenshot('/v3.42/caso17/3-type-member-name');
         cy.get('input[name="email"][id="member-email"]').type(email);
-        cy.screenshot('/v3/caso17/4-type-member-email');
+        cy.screenshot('/v3.42/caso17/4-type-member-email');
         cy.get('textarea[name="note"][id="member-note"]').type('Esta es la descripción del nuevo miembro del Grupo # 11 que será eliminado');
-        cy.screenshot('/v3/caso17/5-type-member-note');
+        cy.screenshot('/v3.42/caso17/5-type-member-note');
         cy.wait(1000);
         cy.get("span").contains("Save").click();
-        cy.screenshot('/v3/caso17/6-click-save');
+        cy.screenshot('/v3.42/caso17/6-click-save');
         cy.wait(3000);
         cy.visit("/#/members");
-        cy.screenshot('/v3/caso17/7-visit-members');
+        cy.screenshot('/v3.42/caso17/7-visit-members');
         cy.wait(1000);
         cy.reload();
         cy.wait(5000);
@@ -83,18 +83,18 @@ describe("Login y escenarios para members", () => {
             cy.wrap(emailElement).invoke('text').then((memberEmailText) => {
                 if (memberEmailText.trim() === email.trim()) {
                 cy.wrap(emailElement).click();
-                cy.screenshot('/v3/caso17/8-click-member-created');
+                cy.screenshot('/v3.42/caso17/8-click-member-created');
                 cy.log(`Se hizo clic en el miembro con el correo electrónico "${email}".`);
                 }
             });
         });
         cy.get("button").contains("Delete member").click();
-        cy.screenshot('/v3/caso17/9-click-delete-member');
+        cy.screenshot('/v3.42/caso17/9-click-delete-member');
         cy.get('.fullscreen-modal-container').should('be.visible');
-        cy.screenshot('/v3/caso17/10-modal-be-visible');
+        cy.screenshot('/v3.42/caso17/10-modal-be-visible');
         cy.wait(3000);
         cy.get('.modal-footer').find('button').contains('Delete member').click();
-        cy.screenshot('/v3/caso17/11-delete-member-confirm');
+        cy.screenshot('/v3.42/caso17/11-delete-member-confirm');
         cy.wait(3000);
         cy.visit("/#/members");
     });
