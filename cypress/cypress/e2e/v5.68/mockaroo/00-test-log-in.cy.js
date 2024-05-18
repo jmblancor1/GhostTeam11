d7df05fc61ff1d78dd3b06333ed0ac14ae5d703f
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-05-18 17:04:44 -0500

    test log in: Ingresar una dirección de correo electrónico no registrado

--- cypress/cypress/e2e/v5.68/mockaroo/00-test-log-in.cy.js
+++ cypress/cypress/e2e/v5.68/mockaroo/00-test-log-in.cy.js
@@ -28,13 +28,23 @@ describe("logueo para acceder a las funcionalidades de ghost con datos a-priori"
     cy.get("#ember6").should("have.value", "");
     cy.get("#ember8").should("have.value", "");
     cy.get("#ember10 > span").click();
-    cy.get(".main-error").should(
-      "contain",
-      "Please fill out the form to sign in."
-    );
+    cy.get(".main-error").should("contain", "Please fill out the form to sign in.");
   });
 
-  it("Ingresar una dirección de correo electrónico y contraseña inválidos", function() {
+  it("Ingresar una dirección de correo electrónico no válido", function () {
+    // Acceder a los datos cargados
+    const testData = this.testData;
+    const randomIndex = getRandomIndex(testData.length);
+    const login = testData[randomIndex]; // Seleccionar un conjunto de datos aleatorio
+
+    // Loguear al usuario
+    cy.get("#ember6").invoke("val", "").type(login.user_name);
+    cy.get("#ember8").invoke("val", "").type(login.password_random);
+    cy.get("#ember10 > span").click();
+    cy.get(".main-error").should("contain", "Please fill out the form to sign in.");
+  });
+
+  it("Ingresar una dirección de correo electrónico no registrado", function() {
     // Acceder a los datos cargados
     const testData = this.testData;
     const randomIndex = getRandomIndex(testData.length);
@@ -44,16 +54,13 @@ describe("logueo para acceder a las funcionalidades de ghost con datos a-priori"
     cy.get("#ember6").invoke("val", "").type(login.email_address_random);
     cy.get("#ember8").invoke("val", "").type(login.password_random);
     cy.get("#ember10 > span").click();
-    // cy.get(".main-error").should("contain", "Please fill out the form to sign in.");
     cy.get(".main-error").should("contain", "There is no user with that email address.");
-    // cy.get(".main-error").should("contain", "Too many login attempts.");
+
   });
 
   it("Ingresar la dirección de correo electrónico y contraseña correctos", function() {
     // Acceder a los datos cargados
     const testData = this.testData;
-
-    // const randomIndex = getRandomIndex(testData.length);
     const login = testData[0]; // Acceder al primer conjunto de datos
 
     // Loguear al usuario
